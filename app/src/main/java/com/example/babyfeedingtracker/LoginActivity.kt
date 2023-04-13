257dd77ceec11f8d230dc8bd45703f360494dd06
Luciano Bracht <lucianoeletronico@gmail.com>
2023-04-13 01:38:06 -0300

    save and load shared preferences in both login and main activities, LoginActivity: validate fields in login activity, show loading checkAvailabilityAndPost(username, stationName, color)

--- app/src/main/java/com/example/babyfeedingtracker/LoginActivity.kt
+++ app/src/main/java/com/example/babyfeedingtracker/LoginActivity.kt
@@ -15,6 +15,15 @@ import androidx.core.content.ContextCompat
 
 import com.jaredrummler.materialspinner.MaterialSpinner
 import com.jaredrummler.materialspinner.MaterialSpinnerBaseAdapter
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import okhttp3.Request
+import java.io.IOException
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
 
 
 class LoginActivity : AppCompatActivity() {
@@ -22,21 +31,19 @@ class LoginActivity : AppCompatActivity() {
     var clickedSquareId: Int = 0
     var hexColor: String = "0"
 
+    //check which square is clicked, if none is clicked, clickedSquareId = 0
     fun onSquareClicked(view: View) {
 
         clickedSquareId = view.id
         Log.d("clicking squares", "clickedSquareId ${clickedSquareId}")
 
         if (clickedSquareId == R.id.purple_square) {
-
             view.background.setColorFilter(
                 ContextCompat.getColor(this, android.R.color.holo_green_light),
                 PorterDuff.Mode.SRC_ATOP
             )
 
             findViewById<View>(R.id.blue_square).background.clearColorFilter()
-
-
             hexColor = Integer.toHexString(ContextCompat.getColor(this, android.R.color.holo_purple))
             // Square 1 was clicked
         } else if (clickedSquareId == R.id.blue_square) {
@@ -47,11 +54,31 @@ class LoginActivity : AppCompatActivity() {
             )
 
             findViewById<View>(R.id.purple_square).background.clearColorFilter()
-
-
             hexColor = Integer.toHexString(ContextCompat.getColor(this, android.R.color.holo_blue_dark))
             // Square 2 was clicked
         }
+    }
+
+    fun validateFieldString(field: String, fieldName: String): String {
+
+        if(field.length < 6) {
+            val toast = Toast.makeText(this, "Please enter at least 6 chars for your $fieldName", Toast.LENGTH_SHORT)
+            toast.show()
+            return "length"
+        }
+
+        val prohibitedChars = " '`|#$*/-,;()\\" + "\n" + '"'
+
+        for(fieldChar in field) {
+            for (prohibitedChar in prohibitedChars) {
+                if (fieldChar == prohibitedChar) {
+                    val toast = Toast.makeText(this,"Please do not use special chars", Toast.LENGTH_SHORT)
+                    toast.show()
+                    return "specialChar"
+                }
+            }
+        }
+        return "valid"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,30 +86,150 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val loginButton: Button = findViewById(R.id.login_button)
+        loginButton.isEnabled = true
+        loginButton.text = "LOGIN"
+
+
         loginButton.setOnClickListener {
+            loginButton.text = "LOADING"
+            loginButton.isEnabled = false
             // Get the data from the EditText fields
             val username: String = findViewById<EditText>(R.id.username_edit_text).text.toString()
             val station: String = findViewById<EditText>(R.id.baby_name_edit_text).text.toString()
+            val userColor: String = hexColor
+
+            var isUsernameValid = false
+            var isStationValid = false
+            var isColorValid = false
+            var isUsernameAvailable = false
+
 
             Log.d("hex color", hexColor)
+            Log.d("username", username)
 
-
-            // TODO: salvar em sharedPreferences e trocar de atividade
-                //não preciso enviar ao db agora, pois vou enviar em cada post
-            /*
-            // Save the data to shared preferences
-            val sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
-            with(sharedPref.edit()) {
-                putString(getString(R.string.saved_name_key), name)
-                putString(getString(R.string.saved_email_key), email)
-                apply()
+            if(validateFieldString(username, "username") == "valid") {
+                isUsernameValid = true
+                if(validateFieldString(station, "baby name") == "valid") {
+                    isStationValid = true
+                    if (userColor == "0") {
+                        val toast =
+                            Toast.makeText(this, "Please choose a color", Toast.LENGTH_SHORT)
+                        toast.show()
+                    } else {
+                        isColorValid = true
+                    }
+                }
             }
 
-            // Go back to the MainActivity
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish() // Finish the SetupActivity so that the user cannot go back to it
-            */
+            if(isColorValid && isStationValid && isUsernameValid) {
+                val toast = Toast.makeText(this, "Everything is valid!", Toast.LENGTH_SHORT)
+                //toast.show()
+
+                fun checkAvailabilityAndPost(username: String, stationName: String, color: String) {
+                    val JSON = "application/json; charset=utf-8".toMediaType()
+                    val url = "https://lbracht-server-bft.glitch.me/station/checkAvailabilityAndPost"
+
+                    val jsonObject = JSONObject().apply {
+                        put("username", username)
+                        put("station", stationName)
+                        put("userColorClient", color)
+                    }
+
+                    val requestBody = jsonObject.toString().toRequestBody(JSON)
+
+                    val client = OkHttpClient()
+
+                    val request = Request.Builder()
+                        .url(url)
+                        .post(requestBody)
+                        .build()
+
+                    val toastError = Toast.makeText(this,"Error checkAvailabilityAndPost" +
+                            "Username: $username" +
+                            "stationName: $stationName" +
+                            "color: $color", Toast.LENGTH_SHORT)
+                    val toastPending = Toast.makeText(this,"User is now pending", Toast.LENGTH_SHORT)
+                    val toastUserNotAvailable = Toast.makeText(this,"Not available username. Please choose another one", Toast.LENGTH_SHORT)
+                    val toastOwner = Toast.makeText(this,"User is now the station's owner", Toast.LENGTH_SHORT)
+
+                    client.newCall(request).enqueue(object : Callback {
+                        override fun onFailure(call: Call, e: IOException) {
+                            Log.d("Error checkAvailabilityAndPost", "onFailure" +
+                                    "Username: $username" +
+                                    "stationName: $stationName" +
+                                    "color: $color")
+                            toastError.show()
+                        }
+
+                        override fun onResponse(call: Call, response: Response) {
+
+                            val responseBodyJSON = JSONObject(response.body!!.string())
+                            Log.d("checkAvailabilityAndPost", "onResponse: $responseBodyJSON")
+                            // TODO: handle pending and owner user cases here
+                            when (responseBodyJSON.getString("message")) {
+                                "userAlreadyInTheStation" -> {
+                                    Log.d("checkAvailabilityAndPost", "Username is not available")
+                                    toastUserNotAvailable.show()
+                                }
+                                "userIsNowPending" -> {
+                                    Log.d("checkAvailabilityAndPost", "User is pending now")
+                                    toastPending.show()
+                                    isUsernameAvailable = true
+                                }
+                                "userIsNowOwner" -> {
+                                    Log.d("checkAvailabilityAndPost", "User is the owner now")
+                                    toastOwner.show()
+                                    isUsernameAvailable = true
+                                }
+                            }
+
+                            if (isUsernameAvailable) {
+                                val loginLocalData =
+                                    getSharedPreferences("login", Context.MODE_PRIVATE)
+                                val loginLocalDataEditor = loginLocalData.edit()
+
+                                loginLocalDataEditor.putString("username", username)
+                                loginLocalDataEditor.putString("userColor", userColor)
+                                loginLocalDataEditor.putString("station", station)
+
+                                loginLocalDataEditor.apply()
+
+                                runOnUiThread {
+                                    loginButton.isEnabled = true
+                                    loginButton.text = "LOGIN"
+                                }
+
+                                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                            } else {
+                                runOnUiThread {
+                                    loginButton.isEnabled = true
+                                    loginButton.text = "LOGIN"
+                                }
+                            }
+                            //}
+                        }
+                    })
+                }
+
+                // done: check here if the names are free to use or if someone else is using them
+                checkAvailabilityAndPost(username, station, userColor)
+                /*
+                if(isUsernameAvailable) {
+                    val loginLocalData = getSharedPreferences("login", Context.MODE_PRIVATE)
+                    val loginLocalDataEditor = loginLocalData.edit()
+
+                    loginLocalDataEditor.putString("username", username)
+                    loginLocalDataEditor.putString("userColor", userColor)
+                    loginLocalDataEditor.putString("station", station)
+
+                    loginLocalDataEditor.apply()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    //finish() // Finish the SetupActivity so that the user cannot go back to it
+                }
+                */
+            }
         }
     }
 }
--- app/src/main/java/com/example/babyfeedingtracker/MainActivity.kt
+++ app/src/main/java/com/example/babyfeedingtracker/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.example.babyfeedingtracker
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
+        val loginLocalData = getSharedPreferences("login", Context.MODE_PRIVATE)
 
+        val username = loginLocalData.getString("username", "noData")
+        val userColor = loginLocalData.getString("userColor", "noData") //Maybe I should put it also in the activity_main.xml, maybe remove it from here
+        val station = loginLocalData.getString("station", "noData")
+
+        if(username == "noData" || station == "noData") {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }

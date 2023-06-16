e7f2c5fa29b37a3dc6df8795fc11b61d0b352e15
Luciano Bracht <lucianoeletronico@gmail.com>
2023-06-16 18:55:27 -0300

    layout activity_main, themes

--- app/src/main/java/com/example/babyfeedingtrackermvvm/view/MainActivity.kt
+++ app/src/main/java/com/example/babyfeedingtrackermvvm/view/MainActivity.kt
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.babyfeedingtrackermvvm.R
 
-// TODO: antes de prosseguir:
-    // TODO: criar arquivo de strings e alimentar com as strings que têm até agora
 
+// TODO: carregar o que precisa aqui do outro código primeiro (layout)
 // TODO: verificar se está logado, se não estiver, mandar para a RegisterActivity
+
+// TODO: para depois:
+    //fazer a register activity ser uma só para registro e login, com um botão de registro e outro de login
+    //se um usuário pendente tentar logar, falar que está pendente e perguntar se quer cancelar
+    //criar logout button
+
+    //do the logic for deactivate login button after click it (new file?)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
